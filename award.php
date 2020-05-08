5e2db3588aec79e846fa52c2bdfa2ddb4a561258
Kay <kayjobs13@.gmail.com>
2020-05-08 14:06:14 +0800

    基本架構

---
+++ award.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php include "./include/header.php" ;?> </title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <? $pageheader="對獎頁面" ;?>
+    <?php include "./layout/topbar.php";?>
+</body>
+</html>
